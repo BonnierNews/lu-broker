9e5be60739679a00031c8e0b4ea853c5c55c01c9
Jens Carlén <jens@neubeck.se>
2019-06-07 13:10:00 +0200

    Triggers in borker

--- test/feature/triggers-feature.js
+++ test/feature/triggers-feature.js
@@ -7,17 +7,6 @@ function handler() {
   return {type: "i-was-here", id: "my-guid"};
 }
 
-function trigger(source) {
-  return {
-    type: "trigger",
-    id: "event.some-name",
-    source,
-    meta: {
-      correlationId: "some-correlation-id"
-    }
-  };
-}
-
 Feature("Triggers", () => {
   const source = {
     type: "order",
@@ -25,6 +14,16 @@ Feature("Triggers", () => {
     meta: {correlationId: "some-correlation-id"},
     attributes: {baz: true}
   };
+  function trigger() {
+    return {
+      type: "trigger",
+      id: "event.some-name",
+      source,
+      meta: {
+        correlationId: "some-correlation-id"
+      }
+    };
+  }
   Scenario("Trigger a flow with a trigger message", () => {
     before(() => {
       crd.resetMock();
@@ -53,6 +52,79 @@ Feature("Triggers", () => {
     And("the flow should be completed", () => {
       flowMessages.length.should.eql(2);
       const {msg, key} = flowMessages.pop();
+      key.should.eql("event.some-name.processed");
+      msg.should.eql({
+        type: "event",
+        id: msg.id,
+        data: [
+          {
+            type: "i-was-here",
+            id: "my-guid",
+            occurredAt: msg.data[0].occurredAt,
+            key: "event.some-name.perform.one"
+          }
+        ],
+        source,
+        meta: {
+          correlationId: "some-correlation-id"
+        }
+      });
+    });
+  });
+
+  Scenario("Trigger a flow by returning a trigger message from handler", () => {
+    before(() => {
+      crd.resetMock();
+      start({
+        recipes: [
+          {
+            namespace: "event",
+            name: "some-other-name",
+            sequence: [route(".perform.one", trigger)]
+          },
+          {
+            namespace: "event",
+            name: "some-name",
+            sequence: [route(".perform.one", handler)]
+          }
+        ]
+      });
+    });
+    let flowMessages;
+    let secondFlowMessages;
+    Given("we are listening for messages on the event namespace", () => {
+      flowMessages = crd.subscribe("event.some-other-name.#");
+      secondFlowMessages = crd.subscribe("event.some-name.#");
+    });
+
+    When("we publish an order on the other events a trigger key", async () => {
+      await crd.publishMessage("trigger.event.some-other-name", source);
+    });
+
+    And("the flow should be completed", () => {
+      flowMessages.length.should.eql(2);
+      const {msg, key} = flowMessages.pop();
+      key.should.eql("event.some-other-name.processed");
+      msg.should.eql({
+        type: "event",
+        id: msg.id,
+        data: [
+          {
+            type: "trigger",
+            id: "event.some-name",
+            occurredAt: msg.data[0].occurredAt,
+            key: "event.some-other-name.perform.one"
+          }
+        ],
+        source,
+        meta: {
+          correlationId: "some-correlation-id"
+        }
+      });
+    });
+    And("the other flow should triggered and be completed", () => {
+      secondFlowMessages.length.should.eql(2);
+      const {msg, key} = secondFlowMessages.pop();
       key.should.eql("event.some-name.processed");
       msg.should.eql({
         type: "event",
