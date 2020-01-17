633a4fe16699b9a980167ee98bc76abbc37f06f3
Jens Carlén <jens@neubeck.se>
2020-01-17 20:08:35 +0100

    WIP: this needs rethinking

--- test/feature/spawn-feature.js
+++ test/feature/spawn-feature.js
@@ -10,11 +10,25 @@ const source = {
   meta: {correlationId: "some-correlation-id"},
   attributes: {baz: true}
 };
+
+const source2 = {...source, id: "some-other-id"};
+
 function trigger() {
   return {
     type: "trigger",
     id: "event.some-sub-name",
     source,
+    meta: {
+      correlationId: "some-correlation-id"
+    }
+  };
+}
+
+function triggerMultiple() {
+  return {
+    type: "trigger",
+    id: "event.some-sub-name",
+    source: [source, source2],
     meta: {
       correlationId: "some-correlation-id"
     }
@@ -135,6 +149,116 @@ Feature("Spawn flows with triggers", () => {
 
     And("the handlers should have been triggered in correct order", () => {
       result.should.eql([0, 1, 2]);
+    });
+  });
+
+  Scenario("Trigger a flow by returning a list of sources in a trigger message from handler", () => {
+    const result = [];
+    function addWithDelay(i, delay = 0) {
+      return async () => {
+        await sleep(delay);
+        result.push(i);
+        return {type: "baz", id: `my-guid-${i}`};
+      };
+    }
+
+    before(() => {
+      crd.resetMock();
+      brokerServer.start();
+      start({
+        recipes: [
+          {
+            namespace: "event",
+            name: "some-name",
+            sequence: [
+              route(".perform.first", addWithDelay(0, 1)),
+              route(".perform.one", triggerMultiple),
+              route(".perform.two", addWithDelay(2, 1))
+            ]
+          },
+          {
+            namespace: "event",
+            name: "some-sub-name",
+            sequence: [route(".perform.one", addWithDelay(1, 5))]
+          }
+        ]
+      });
+    });
+
+    after(() => {
+      brokerServer.reset();
+    });
+    let flowMessages, donePromise, internalMessages;
+    Given("we are listening for messages on the event namespace", () => {
+      flowMessages = crd.subscribe("event.some-name.#");
+      internalMessages = crd.subscribe("#.internal.#");
+      donePromise = new Promise((resolve) => crd.subscribe("event.some-name.processed", resolve));
+    });
+
+    When("we publish an order on the other events a trigger key", async () => {
+      await crd.publishMessage("trigger.event.some-name", source);
+    });
+
+    Then("we should get 2 internal trigger messages", () => {
+      internalMessages.should.have.length(2);
+    });
+
+    And("the last one should be the last source message", () => {
+      const {key, msg} = internalMessages[1];
+      key.should.eql("lu-broker.internal.trigger-message");
+      msg.id.should.eql("event.some-name.perform.one:some-correlation-id");
+      msg.attributes.should.eql({
+        responseKey: "event.some-name.perform.two",
+        trigger: "trigger.event.some-sub-name",
+        source: source2,
+        message: {
+          id: flowMessages[0].msg.id,
+          type: "event",
+          data: [
+            {
+              id: "my-guid-0",
+              key: "event.some-name.perform.first",
+              occurredAt: msg.attributes.message.data[0].occurredAt,
+              type: "baz"
+            },
+            {
+              id: "event.some-sub-name",
+              key: "event.some-name.perform.one",
+              occurredAt: msg.attributes.message.data[1].occurredAt,
+              type: "trigger"
+            },
+            {
+              id: "event.some-sub-name",
+              key: "event.some-name.perform.one",
+              occurredAt: msg.attributes.message.data[2].occurredAt,
+              type: "trigger"
+            }
+          ],
+          source: {id: source.id, type: source.type, attributes: source.attributes},
+          meta: {
+            correlationId: "some-correlation-id"
+          }
+        }
+      });
+    });
+
+    And("the flow should be completed", async () => {
+      await donePromise;
+      flowMessages.length.should.eql(6);
+      const {msg, key} = flowMessages.pop();
+      key.should.eql("event.some-name.processed");
+      msg.data
+        .map(({type, id}) => ({type, id}))
+        .should.eql([
+          {type: "baz", id: "my-guid-0"},
+          {type: "trigger", id: "event.some-sub-name"},
+          {type: "trigger", id: "event.some-sub-name"},
+          {type: "baz", id: "my-guid-2"}
+        ]);
+    });
+
+    And("the handlers should have been triggered in correct order", () => {
+      result.should.eql([0, 1, 1, 2, 2]);
     });
   });
 
