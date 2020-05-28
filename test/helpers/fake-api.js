eac43902022096b6d595097c7fb2e312962f9d97
Jens Carlén <jens@neubeck.se>
2020-05-28 12:43:05 +0200

    Http tests for job store

--- test/helpers/fake-api.js
+++ test/helpers/fake-api.js
@@ -23,9 +23,45 @@ function hasPendingCall(apiPath) {
     }) !== undefined
   );
 }
+
+function mount(testData) {
+  if (Array.isArray(testData)) {
+    return testData.map(mount);
+  }
+  let actualBody;
+  const {request} = testData;
+  const mock = api[request.method.toLowerCase()](request.path, (body) => {
+    actualBody = body;
+    return true;
+  });
+
+  if (request.query) {
+    mock.query(request.query);
+  }
+
+  if (request.headers) {
+    for (const [key, val] of Object.entries(request.headers)) {
+      mock.matchHeader(key, val);
+    }
+  }
+
+  if (testData.times) {
+    mock.times(testData.times);
+  }
+
+  mock.reply(testData.statusCode || testData.status || 200, testData.body);
+  return {
+    api: mock,
+    hasExpectedBody: (body) => {
+      return actualBody.should.eql(body || request.body);
+    },
+    postedBody: () => actualBody
+  };
+}
 module.exports = {
   api,
   pendingMocks: api.pendingMocks.bind(api),
+  mount,
   get: api.get.bind(api),
   post: api.post.bind(api),
   delete: api.delete.bind(api),
