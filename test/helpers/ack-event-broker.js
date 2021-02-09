18ced9ac571981c811c7c793ba857630a14690ac
Jens Carlén <jens@neubeck.se>
2021-02-09 20:52:05 +0100

    Close enough for testz

--- test/helpers/ack-event-broker.js
+++ test/helpers/ack-event-broker.js
@@ -8,31 +8,30 @@ function newSub(routingKeyOrKeys, queue, handler, cb) {
   const functions = [];
   let locked = false;
 
-  function run(fn) {
-    if (locked) {
-      functions.push(fn);
-      return;
-    } else if (fn) {
+  function run() {
+    if (!locked && functions.length > 0) {
       locked = true;
+      const fn = functions.shift();
       fn();
     }
   }
 
   const oldHandler = handler;
   handler = (message, meta, notify) => {
+    functions.push(() => oldHandler(message, meta, notify));
     const oldAck = notify.ack;
     const oldNack = notify.nack;
     notify.ack = () => {
       oldAck();
       locked = false;
-      run(functions.shift());
+      run();
     };
     notify.nack = (...args) => {
       oldNack(...args);
       locked = false;
-      run(functions.shift());
+      run();
     };
-    run(() => oldHandler(message, meta, notify));
+    run();
   };
   oldSub(routingKeyOrKeys, queue, handler, cb);
 }
