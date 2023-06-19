a58808fd503a05ae55cad05dc576f6e1b22ca482
Simon Varney <varney@users.noreply.github.com>
2023-06-19 11:19:14 +0200

    Test using request instead of axios again

--- liveness.js
+++ liveness.js
@@ -1,24 +1,21 @@
 "use strict";
 
 const config = require("exp-config");
-const axios = require("axios");
-const {logger} = require("lu-logger");
-const callingAppName = require(`${process.cwd()}/package.json`).name;
 
-async function rabbitStatus() {
-  try {
-    const response = await axios({
-      url: `${config.rabbit.apiUrl}/api/connections`,
-      method: "get"
+const request = require("request");
+
+function rabbitStatus() {
+  return new Promise((resolve, reject) => {
+    request.get(`${config.rabbit.apiUrl}/api/connections`, (err, response, body) => {
+      if (err) return reject(err);
+      if (response.statusCode !== 200) return reject(new Error(response.statusCode));
+      const myConn = JSON.parse(body).find((conn) => conn.client_properties.connection_name === config.HOSTNAME);
+      if (!myConn) return reject(new Error(`Could not find rabbit connection for: ${config.HOSTNAME}`));
+      resolve();
     });
-    if (response.status !== 200) throw new Error(response.status);
-    const myConn = response.data.find((conn) => conn.client_properties.connection_name === config.HOSTNAME);
-    if (!myConn) throw new Error(`Could not find rabbit connection for: ${config.HOSTNAME}`);
-    return 0;
-  } catch (err) {
-    logger.error("Liveness failed - Rabbit Error", err, {meta: {requesterName: callingAppName}});
-    return 1;
-  }
+  })
+    .then(() => 0)
+    .catch(() => 1);
 }
 
 async function cli() {
